e608f13f3df083f6d5c9f6357ffd7084905de6eb
Tibo Lousee <l33t@online.de>
2011-12-28 22:29:49 +0100

    Project Files

---
+++ YoutubeDownloadNew/Form1.Designer.vb
@@ -0,0 +1,170 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Das Formular überschreibt den Löschvorgang, um die Komponentenliste zu bereinigen.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Wird vom Windows Form-Designer benötigt.
+    Private components As System.ComponentModel.IContainer
+
+    'Hinweis: Die folgende Prozedur ist für den Windows Form-Designer erforderlich.
+    'Das Bearbeiten ist mit dem Windows Form-Designer möglich.  
+    'Das Bearbeiten mit dem Code-Editor ist nicht möglich.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.WebClient1 = New System.Net.WebClient()
+        Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.SaveFileDialog1 = New System.Windows.Forms.SaveFileDialog()
+        Me.CommandButton1 = New dotNetBase.Windows.Forms.commandButton()
+        Me.WaitingCircle1 = New dotNetBase.Windows.Forms.Aero.waitingCircle()
+        Me.BackgroundWorker1 = New System.ComponentModel.BackgroundWorker()
+        Me.PromptedTextBox1 = New dotNetBase.Windows.Forms.promptedTextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'WebClient1
+        '
+        Me.WebClient1.BaseAddress = ""
+        Me.WebClient1.CachePolicy = Nothing
+        Me.WebClient1.Credentials = Nothing
+        Me.WebClient1.Encoding = CType(resources.GetObject("WebClient1.Encoding"), System.Text.Encoding)
+        Me.WebClient1.Headers = CType(resources.GetObject("WebClient1.Headers"), System.Net.WebHeaderCollection)
+        Me.WebClient1.QueryString = CType(resources.GetObject("WebClient1.QueryString"), System.Collections.Specialized.NameValueCollection)
+        Me.WebClient1.UseDefaultCredentials = False
+        '
+        'ProgressBar1
+        '
+        Me.ProgressBar1.Location = New System.Drawing.Point(186, 80)
+        Me.ProgressBar1.Name = "ProgressBar1"
+        Me.ProgressBar1.Size = New System.Drawing.Size(329, 23)
+        Me.ProgressBar1.TabIndex = 0
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.Checked = True
+        Me.CheckBox1.CheckState = System.Windows.Forms.CheckState.Checked
+        Me.CheckBox1.Font = New System.Drawing.Font("Segoe UI", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.CheckBox1.Location = New System.Drawing.Point(186, 51)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(161, 23)
+        Me.CheckBox1.TabIndex = 3
+        Me.CheckBox1.Text = "MP3 Konvertierung"
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
+        'SaveFileDialog1
+        '
+        '
+        'CommandButton1
+        '
+        Me.CommandButton1.commandButtonIcon = dotNetBase.Windows.Forms.commandButtonIcons.Arrow
+        Me.CommandButton1.Description = "Lädt das Video herunter"
+        Me.CommandButton1.FlatStyle = System.Windows.Forms.FlatStyle.System
+        Me.CommandButton1.Location = New System.Drawing.Point(12, 51)
+        Me.CommandButton1.Name = "CommandButton1"
+        Me.CommandButton1.Size = New System.Drawing.Size(168, 52)
+        Me.CommandButton1.TabIndex = 4
+        Me.CommandButton1.Text = "Download"
+        '
+        'WaitingCircle1
+        '
+        Me.WaitingCircle1.Location = New System.Drawing.Point(499, 58)
+        Me.WaitingCircle1.MaximumSize = New System.Drawing.Size(16, 16)
+        Me.WaitingCircle1.MinimumSize = New System.Drawing.Size(16, 16)
+        Me.WaitingCircle1.Name = "WaitingCircle1"
+        Me.WaitingCircle1.Size = New System.Drawing.Size(16, 16)
+        Me.WaitingCircle1.TabIndex = 5
+        Me.WaitingCircle1.Text = "WaitingCircle1"
+        Me.WaitingCircle1.Visible = False
+        '
+        'BackgroundWorker1
+        '
+        '
+        'PromptedTextBox1
+        '
+        Me.PromptedTextBox1.Font = New System.Drawing.Font("Segoe UI Light", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.PromptedTextBox1.Location = New System.Drawing.Point(12, 12)
+        Me.PromptedTextBox1.Name = "PromptedTextBox1"
+        Me.PromptedTextBox1.PromptText = "Youtube URL"
+        Me.PromptedTextBox1.Size = New System.Drawing.Size(503, 33)
+        Me.PromptedTextBox1.TabIndex = 6
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Enabled = False
+        Me.Label1.Location = New System.Drawing.Point(359, 106)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(156, 13)
+        Me.Label1.TabIndex = 7
+        Me.Label1.Text = "Version 1.0 ©2011 Validati0n"
+        '
+        'Label2
+        '
+        Me.Label2.Location = New System.Drawing.Point(353, 54)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(162, 23)
+        Me.Label2.TabIndex = 8
+        Me.Label2.Text = "0%"
+        Me.Label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(12, 106)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(47, 13)
+        Me.Label3.TabIndex = 9
+        Me.Label3.Text = "Warte..."
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(523, 127)
+        Me.Controls.Add(Me.WaitingCircle1)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.PromptedTextBox1)
+        Me.Controls.Add(Me.CommandButton1)
+        Me.Controls.Add(Me.CheckBox1)
+        Me.Controls.Add(Me.ProgressBar1)
+        Me.Font = New System.Drawing.Font("Segoe UI", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Youtube Downloader"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents WebClient1 As System.Net.WebClient
+    Friend WithEvents ProgressBar1 As System.Windows.Forms.ProgressBar
+    Friend WithEvents CheckBox1 As System.Windows.Forms.CheckBox
+    Friend WithEvents SaveFileDialog1 As System.Windows.Forms.SaveFileDialog
+    Friend WithEvents CommandButton1 As dotNetBase.Windows.Forms.commandButton
+    Friend WithEvents WaitingCircle1 As dotNetBase.Windows.Forms.Aero.waitingCircle
+    Friend WithEvents BackgroundWorker1 As System.ComponentModel.BackgroundWorker
+    Friend WithEvents PromptedTextBox1 As dotNetBase.Windows.Forms.promptedTextBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+
+End Class
---
+++ YoutubeDownloadNew/Form1.vb
@@ -0,0 +1,110 @@
+﻿Option Strict On
+Imports System.IO
+Public Class Form1
+    Dim Tempfile As String = My.Computer.FileSystem.SpecialDirectories.Temp & "\tempvideop1.flv"
+    Dim Tempfile2 As String = My.Computer.FileSystem.SpecialDirectories.Temp & "\tempsong.mp3"
+    Dim test As Boolean
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        WaitingCircle1.Start()
+        Control.CheckForIllegalCrossThreadCalls = False
+    End Sub
+
+    Private Sub CommandButton1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles CommandButton1.Click
+        Label3.Text = "Initialisiere..."
+        If PromptedTextBox1.Text.Contains("youtube.com/watch") = True Then
+            If CheckBox1.Checked = True Then
+                SaveFileDialog1.Filter = "MP3-Audio Datei|*.mp3"
+            Else
+                SaveFileDialog1.Filter = "Web Video Datei|*.webm"
+            End If
+            SaveFileDialog1.ShowDialog()
+        Else
+            dotNetBase.Windows.Forms.messageBoxEx.Show(Me, "Fehler", "Ungültiger URL", "Der eingegebene URL ist nicht gültig, bitte überprüfen sie diesen.", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            Label3.Text = "Warte..."
+        End If
+    End Sub
+
+    Private Sub BackgroundWorker1_DoWork(ByVal sender As System.Object, ByVal e As System.ComponentModel.DoWorkEventArgs) Handles BackgroundWorker1.DoWork
+        Label3.Text = "Suche Downloadlink..."
+        CheckBox1.Enabled = False
+        PromptedTextBox1.Enabled = False
+        CommandButton1.Enabled = False
+        Me.Cursor = Cursors.WaitCursor
+        Dim inhalt As String = WebClient1.DownloadString(New Uri(PromptedTextBox1.Text))
+        Dim pos1 As Integer = inhalt.IndexOf("url_encoded_fmt_stream_map=url%3D")
+        Dim link1 As String = inhalt.Substring(pos1 + 33)
+        Dim pos2 As Integer = link1.IndexOf("%26fallback_host")
+        link1 = link1.Substring(0, pos2)
+        link1 = link1.Replace("%3F", "?").Replace("%3D", "=").Replace("%26", "&").Replace("%25", "%").Replace("%2C", ",").Replace("%25", "%").Replace("%3A", ":").Replace("%2F", "/")
+        link1 = link1.Replace("%3F", "?").Replace("%3D", "=").Replace("%26", "&").Replace("%25", "%").Replace("%2C", ",").Replace("%25", "%").Replace("%3A", ":").Replace("%2F", "/")
+        If IO.File.Exists(Tempfile) = True Then
+            Kill(Tempfile)
+        End If
+        If IO.File.Exists(Tempfile2) = True Then
+            Kill(Tempfile2)
+        End If
+        Label3.Text = "Download..."
+        WebClient1.DownloadFileAsync(New Uri(link1), Tempfile)
+    End Sub
+
+    Private Sub SaveFileDialog1_FileOk(ByVal sender As System.Object, ByVal e As System.ComponentModel.CancelEventArgs) Handles SaveFileDialog1.FileOk
+        BackgroundWorker1.RunWorkerAsync()
+    End Sub
+
+    Private Sub WebClient1_DownloadProgressChanged(ByVal sender As System.Object, ByVal e As System.Net.DownloadProgressChangedEventArgs) Handles WebClient1.DownloadProgressChanged
+        ProgressBar1.Value = e.ProgressPercentage
+        Label2.Text = e.ProgressPercentage & "%"
+    End Sub
+
+    Private Sub WebClient1_DownloadFileCompleted(ByVal sender As System.Object, ByVal e As System.ComponentModel.AsyncCompletedEventArgs) Handles WebClient1.DownloadFileCompleted
+        ProgressBar1.Value = 0
+        If CheckBox1.Checked = False Then
+            Label3.Text = "Kopiere..."
+            FileCopy(Tempfile, SaveFileDialog1.FileName)
+            Dim i As Integer = dotNetBase.Windows.Forms.messageBoxEx.Show(Me, "Download erfolgreich", "Video Download erfolgreich", "Das Video wurde erfolgreich heruntergeladen, wollen sie dieses nun öffnen ? ", MessageBoxButtons.YesNo, MessageBoxIcon.Question)
+            If i = 6 Then
+                Process.Start(SaveFileDialog1.FileName)
+            End If
+        Else
+            Label3.Text = "Konvertiere..."
+            Dim binaryw As New BinaryWriter(New FileStream("ffmpeg.exe", FileMode.Create))
+            binaryw.Write(My.Resources.ffmpeg)
+            binaryw.Close()
+            Dim fs As New IO.StreamWriter("test.bat", False)
+            fs.WriteLine("ffmpeg.exe -i " & Chr(34) & Tempfile & Chr(34) & _
+                         " -ab 160000 -acodec libmp3lame " & Chr(34) & _
+                        Tempfile2 & Chr(34))
+            fs.WriteLine("del " + Chr(34) + "ffmpeg.exe" + Chr(34))
+            fs.WriteLine("del " + Chr(34) + "video.flv" + Chr(34))
+            fs.WriteLine("del " + Chr(34) + "test.bat" + Chr(34))
+            fs.Close()
+            Dim pinfo As New ProcessStartInfo
+            pinfo.UseShellExecute = False
+            pinfo.CreateNoWindow = True
+            pinfo.FileName = "test.bat"
+            Dim p As Process = Process.Start(pinfo)
+            test = True
+            Do
+                If Len(Dir("test.bat")) = 0 Then
+                    test = False
+                    Label3.Text = "Kopiere..."
+                    FileCopy(Tempfile2, SaveFileDialog1.FileName)
+                    Dim i As Integer = dotNetBase.Windows.Forms.messageBoxEx.Show(Me, "Download erfolgreich", "Video Download erfolgreich", "Das Video wurde erfolgreich heruntergeladen, wollen sie dieses nun öffnen ? ", MessageBoxButtons.YesNo, MessageBoxIcon.Question)
+                    If i = 6 Then
+                        Process.Start(SaveFileDialog1.FileName)
+                    End If
+                    Exit Do
+                Else
+                End If
+            Loop
+        End If
+        PromptedTextBox1.Text = ""
+        CommandButton1.Enabled = True
+        CheckBox1.Enabled = True
+        PromptedTextBox1.Enabled = True
+        Label2.Text = "0%"
+        Label3.Text = "Warte..."
+        Me.Cursor = Cursors.Default
+    End Sub
+End Class
